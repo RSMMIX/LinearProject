8dfa2f7767a0dac947e924c4379ea107e961bb79
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-10-09 23:52:41 +0700

    first commit

---
+++ Project.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import missingno as msno
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler , Normalizer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from scipy.stats import norm
+from scipy import stats
+from sklearn import metrics
+import warnings
+# warnings.filterwarnings('ignore')
+# %matplotlib inline
